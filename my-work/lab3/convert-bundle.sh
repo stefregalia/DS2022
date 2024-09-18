95292366f0fa9565149d014a6fbc5894e656e65a
stefregalia <xtm9px@virginia.edu>
2024-09-18 18:53:47 -0400

    finished question 2

--- my-work/lab3/convert-bundle.sh
+++ my-work/lab3/convert-bundle.sh
@@ -1,1 +1,15 @@
-hi shrimpy
+#!/bin/bash
+
+curl -O https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
+
+tar -xzf lab3-bundle.tar.gz
+
+tr -s '\n' > lab3.tsv  
+
+tr '\t' ',' < lab3.tsv > lab3.csv
+
+num_lines=$(tail -n +2 lab3.csv | wc -l)
+
+echo "The total number of lines remaining in the data file is $num_lines"
+
+tar -czf converted-archive.tar.gz lab3.csv
