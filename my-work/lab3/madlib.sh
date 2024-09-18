488a8b68f2c5d8705b1e5ad1a769f337e5a432c4
stefregalia <xtm9px@virginia.edu>
2024-09-18 18:25:58 -0400

    changes

---
+++ my-work/lab3/madlib.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an unusual sport: " NOUN1
+read -p "2. Name your favorite verb: " VERB1
+read -p "3. Name how often you sleep: " ADVERB1
+read -p "4. Name a weird thing to eat: "  NOUN2
+read -p "5. Name a word to describe food: " ADJECTIVE1
+read -p "6. Name a word to describe school: " ADJECTIVE2
+read -p "7. Name a weird vacation spot: " NOUN3
+read -p "8. Name a weird thing to do to a ball: " VERB2
+
+echo "Once upon a time, my brother played $NOUN1. His favorite thing to do
+was $VERB1 the Golden Snitch during games. If he didn't $VERB1 the Golden
+Snitch, he would $VERB2 it instead. He would train so much, he would
+sleep $ADVERB1 and only eat $NOUN2. The $NOUN2 was so $ADJECTIVE1.
