e305c93fc31016e4c1913abe983ca62dabb1f278
stefregalia <xtm9px@virginia.edu>
2024-09-18 19:05:22 -0400

    updated question 2

--- my-work/lab3/convert-bundle.sh
+++ my-work/lab3/convert-bundle.sh
@@ -4,9 +4,19 @@ curl -O https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
 
 tar -xzf lab3-bundle.tar.gz
 
-tr -s '\n' > lab3.tsv  
+#!/bin/bash
 
-tr '\t' ',' < lab3.tsv > lab3.csv
+
+curl -O 
+#!/bin/bash
+
+curl -O https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
+
+tar -xzf lab3-bundle.tar.gz
+
+cat lab3.tsv | tr -s '\n' > new_lab3.tsv
+
+tr '\t' ',' < new_lab3.tsv > lab3.csv
 
 num_lines=$(tail -n +2 lab3.csv | wc -l)
 
